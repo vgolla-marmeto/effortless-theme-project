44b08ee5b4016de8cbbb29acfc2640ec9d21ef22
unknown <venkateshgolla134@gmail.com>
2024-04-30 10:39:46 +0530

    change

--- assets/practice-custom.js
+++ assets/practice-custom.js
@@ -4,12 +4,19 @@ class customComponent extends HTMLElement{
     this.element = this
     console.log(this.element)
     this.addEventListener("change", (event) => {
-        variantChange()
-    
+        if (event.target.name == "options-change") this.variantChange();
       });
   }
   variantChange(){
-    
+     this.selectedVariantId = this.querySelector('[name="options-change"]').value
+     let dataUrl = `products/${this.dataset.handle}?view="practice-product-card"&variant=${this.selectedVariantId}`
+     fetch(dataUrl).then((response) => response.text()).then((responseText) => {
+        const html = new DOMParser().parseFromString(responseText, 'text/html');
+        const responseCard = html.querySelector('custom-product-card');
+        console.log(responseCard)
+        this.innerHTML = responseCard.innerHTML;
+   
+     })
   }
 
 }
